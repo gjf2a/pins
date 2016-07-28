e44ba95e67d6d1284b1286878436ca3ea7cf433a
Gabriel J. Ferrer <gabriel@Gabriels-MacBook-Pro-2.local>
2016-07-27 22:46:58 -0500

    Version that parses all four types of line compiles.

--- src/config_robot.rs
+++ src/config_robot.rs
@@ -2,7 +2,7 @@ use std::io;
 use std::path::Path;
 use std::io::BufReader;
 use std::io::BufRead;
-use std::io::Error;
+use std::error::Error;
 use std::io::ErrorKind;
 use std::fs::File;
 use std::str::SplitWhitespace;
@@ -56,8 +56,42 @@ fn process_contents(keyword: &str, contents: &mut SplitWhitespace, config: io::R
 					},
 					Err(err) => Err(err),
 				}
+			} else if keyword == "ir" {
+				match contents.next() {
+					Some(ir_str) => {
+						let mut updated = copy(config.ir_sensors);
+						match ir_str.parse::<u32>() {
+							Ok(value) => {
+								updated.push(value);
+								Ok(RobotConfig {motors: config.motors, ir_sensors: updated, steps_per_sec: config.steps_per_sec, max_steps: config.max_steps})
+							},
+							Err(err) => make_error_msg(err.description()),
+						}
+					},
+					None => make_error_msg("No pin specified"),
+				}
+			} else if keyword == "hz" {
+				match contents.next() {
+					Some(hz_str) => {
+						match hz_str.parse::<u32>() {
+							Ok(value) => Ok(RobotConfig {motors: config.motors, ir_sensors: config.ir_sensors, steps_per_sec: value, max_steps: config.max_steps}),
+							Err(err) => make_error_msg(err.description()),
+						}
+					},
+					None => make_error_msg("Cycles per second not specified"),
+				}
+			} else if keyword == "max" {
+				match contents.next() {
+					Some(max_str) => {
+						match max_str.parse::<u32>() {
+							Ok(value) => Ok(RobotConfig {motors: config.motors, ir_sensors: config.ir_sensors, steps_per_sec: config.steps_per_sec, max_steps: Some(value)}),
+							Err(err) => make_error_msg(err.description()),
+						}
+					},
+					None => make_error_msg("Maximum steps not specified"),
+				}
 			} else {
-				Err(Error::new(ErrorKind::InvalidInput, format!("Unrecognized keyword: {}", keyword)))
+				make_error_msg(&format!("Unrecognized keyword: {}", keyword))
 			}
 		},
 		Err(err) => Err(err),
@@ -70,14 +104,18 @@ fn array_from(contents: &mut SplitWhitespace) -> io::Result<[String; 4]> {
 		Ok([String::from(contents[0]), String::from(contents[1]),
 		String::from(contents[2]), String::from(contents[3])])
 	} else {
-		Err(Error::new(ErrorKind::InvalidInput, format!("4 pins expected; received {}", contents.len())))
+		make_error_msg(&format!("4 pins expected; received {}", contents.len()))
 	}
 }
 
-fn copy(v: Vec<[String; 4]>) -> Vec<[String; 4]> {
+fn copy<T>(v: Vec<T>) -> Vec<T> {
 	let mut result = Vec::new();
 	for arr in v {
 		result.push(arr);
 	}
 	result
+}
+
+fn make_error_msg<T>(msg: &str) -> io::Result<T> {
+	Err(io::Error::new(ErrorKind::InvalidInput, msg))
 }
